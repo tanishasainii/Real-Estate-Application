d777256f6f38bfa9ce5097c77afad6f4e68d8bf3
tanishasainii <sainitanisha967@gmail.com>
2024-07-19 01:38:35 +0530

    commit

--- Client/src/components/Header.jsx
+++ Client/src/components/Header.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import {FaSearch} from 'react-icons/fa';
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    
     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Property</span>
-            <span className='text-slate-700'>Pioneer</span>
-          </h1>
-          <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+          <span className='text-slate-500'>Property</span>
+          <span className='text-slate-700'>Pioneer</span>
+        </h1>
+        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
           <input
             type='text'
             placeholder='Search...'
-            className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-         
-            <FaSearch className='text-slate-600' />
-       
+            className='bg-transparent focus:outline-none w-24 sm:w-64'
+          />
+
+          <FaSearch className='text-slate-600' />
         </form>
         <ul className='flex gap-4'>
           <Link to='/'>
@@ -30,24 +29,14 @@ function Header() {
             <li className='hidden sm:inline text-slate-700 hover:underline'>
               About
             </li>
-            </Link>
-            <Link to='/sign-in'>
-            <li className=' text-slate-700 hover:underline'>
-              Sign in
-            </li>
-            </Link>
-
-            </ul>
-
-
-
-
-
-
-
+          </Link>
+          <Link to='/sign-in'>
+            <li className=' text-slate-700 hover:underline'>Sign in</li>
+          </Link>
+        </ul>
       </div>
-      </header>
-  )
+    </header>
+  );
 }
 
-export default Header
+export default Header;
