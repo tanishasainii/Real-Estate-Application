92725514985af660b132407a2c76d672285ebf0a
tanishasainii <sainitanisha967@gmail.com>
2024-07-20 18:54:47 +0530

    database connection

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
-app.listen(3000, ()=>{
-  console.log("server is running on port 3000");
-} )
+dotenv.config();
 
+const mongoURI = process.env.MONGODB;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Database Connected!!');
+}).catch((err) => {
+  console.log('Database connection error:', err);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
