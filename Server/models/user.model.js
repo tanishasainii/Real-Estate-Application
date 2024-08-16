c4ea999af406f7fdb3e0e8c5f19bfed9a199e342
tanishasainii <sainitanisha967@gmail.com>
2024-08-16 14:05:22 +0530

    model created

--- Server/models/user.model.js
+++ Server/models/user.model.js
@@ -1,5 +1,4 @@
-import mongoose from 'mongoose';
-
+const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     username: {
@@ -15,10 +14,6 @@ const userSchema = new mongoose.Schema(
     password: {
       type: String,
       required: true,
-    },
-    avatar:{
-      type: String,
-      default: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
     },
   },
   { timestamps: true }
