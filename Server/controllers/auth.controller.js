1dd7326c9558eb7931e815f3fef2b044d106d9c1
tanishasainii <sainitanisha967@gmail.com>
2024-08-24 23:17:27 +0530

    Handling errors

--- Server/controllers/auth.controller.js
+++ Server/controllers/auth.controller.js
@@ -1,7 +1,9 @@
 const User = require('../models/user.model.js');
 const bcrypt = require('bcryptjs');
+const errorHandler = require('../utils/error.js');
 
-const signup = async (req, res) => {
+
+const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashedPassword = bcrypt.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashedPassword });
@@ -10,9 +12,10 @@ const signup = async (req, res) => {
     await newUser.save();
     res.status(201).json('User created successfully!');
   } catch (error) {
-    res.status(500).json(error.message);
-  }
+    next(error);
+}
 };
+
 
 module.exports = {
   signup
--- Server/index.js
+++ Server/index.js
@@ -25,6 +25,26 @@ mongoose.connect(mongoURI, {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
