ac36a4c44885b5133f53a16a942169ad92e8e2d7
tanishasainii <sainitanisha967@gmail.com>
2024-08-25 19:05:06 +0530

    SignIn API route

--- Server/index.js
+++ Server/index.js
@@ -35,15 +35,6 @@ app.use((err, req, res, next) => {
   });
 });
 
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-});
 
 
 app.listen(3000, () => {
--- Server/routes/auth.route.js
+++ Server/routes/auth.route.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { signup } = require('../controllers/auth.controller.js');
+const {signIn, signup } = require('../controllers/auth.controller.js');
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signIn", signIn);
+
 module.exports = router;
 
