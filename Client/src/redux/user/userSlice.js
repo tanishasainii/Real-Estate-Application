3d836062bad29258be8f0d5d3bded997caedeaa1
tanishasainii <sainitanisha967@gmail.com>
2024-08-27 17:47:06 +0530

    Delete user functionality

--- Client/src/redux/user/userSlice.js
+++ Client/src/redux/user/userSlice.js
@@ -34,7 +34,19 @@ const userSlice = createSlice({
     updateUserFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
-  }
+  },
+  deleteUserStart: (state) => {
+    state.loading = true;
+  },
+  deleteUserSuccess: (state) => {
+    state.currentUser = null;
+    state.loading = false;
+    state.error = null;
+  },
+  deleteUserFailure: (state, action) => {
+    state.error = action.payload;
+    state.loading = false;
+  },
 });
 
 
@@ -45,6 +57,10 @@ export const {
       updateUserFailure,
       updateUserSuccess,
       updateUserStart,
+      deleteUserFailure,
+      deleteUserSuccess,
+      deleteUserStart,
+
     } = userSlice.actions;
 
     export default userSlice.reducer;
--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { test, updateUser } = require('../controllers/user.controller.js');
+const { test, updateUser, deleteUser } = require('../controllers/user.controller.js');
 
 const router= express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
-
+router.delete('/delete/:id', verifyToken, deleteUser)
 module.exports = router;
 
