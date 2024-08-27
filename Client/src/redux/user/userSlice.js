2608dac69496ef2360a461b29459254c8389c01d
tanishasainii <sainitanisha967@gmail.com>
2024-08-27 17:38:28 +0530

    update user

--- Client/src/redux/user/userSlice.js
+++ Client/src/redux/user/userSlice.js
@@ -21,7 +21,19 @@ const userSlice = createSlice({
     signInFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
-    }
+    },
+    
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
   }
 });
 
@@ -29,7 +41,10 @@ const userSlice = createSlice({
 export const {
       signInStart,
       signInSuccess,
-      signInFailure
+      signInFailure,
+      updateUserFailure,
+      updateUserSuccess,
+      updateUserStart,
     } = userSlice.actions;
 
     export default userSlice.reducer;
