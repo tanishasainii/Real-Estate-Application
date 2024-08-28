5fa85d1d20ecedca3cb60f208a646c2e973c7dce
tanishasainii <sainitanisha967@gmail.com>
2024-08-28 17:28:13 +0530

    Update Listing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import SignIn from './pages/SignIn'
@@ -7,6 +7,8 @@ import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing';
 
 function App() {
   return (
@@ -19,6 +21,11 @@ function App() {
     <Route path='/sign-up' element={<SignUp/>} />
     <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route
+            path='/update-listing/:listingId'
+            element={<UpdateListing />}
+          />
     </Route>
     </Route>
     
--- Server/routes/listing.route.js
+++ Server/routes/listing.route.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {createListing, deleteListing, updateListing} = require('../controllers/listing.controller.js');
+const {createListing, deleteListing, updateListing, getListing} = require('../controllers/listing.controller.js');
 const verifyToken = require('../utils/verifyUser.js');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
 router.post('/update/:id', verifyToken, updateListing);
+router.get('/get/:id', getListing);
 
 
 export default router;
