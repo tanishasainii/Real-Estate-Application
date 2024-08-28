49cab7f5b9a3f758797fb4b2e8b2168b223e50bb
tanishasainii <sainitanisha967@gmail.com>
2024-08-28 16:43:01 +0530

    listing API route

--- Server/routes/user.route.js
+++ Server/routes/user.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { test, updateUser, deleteUser } = require('../controllers/user.controller.js');
+const { test, updateUser, deleteUser, getUserListings } = require('../controllers/user.controller.js');
 
 const router= express.Router();
 
 router.get('/test', test);
 router.post('/update/:id', verifyToken, updateUser)
 router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 module.exports = router;
 
